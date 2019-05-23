b9840d0c58972e23f9f63785c50182231d333092
kyoya.takei <kyoya-takei@dac.co.jp>
2019-05-23 03:50:36 +0000

    modified welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-    @if (Auth::check())
-        aaaaaaaa
-    @else
-        <div class="center jumbotron">
-            <div class="text-center">
-                <h1>Welcome to task management application!!</h1>
-                {!! link_to_route('signup.get', 'Sign up Now!!', [], ['class' => 'btn btn-primary']) !!}
-            </div>
+    <div class="center jumbotron">
+        <div class="text-center">
+            <h1>Welcome to task management application!!</h1>
+            {!! link_to_route('signup.get', 'Sign up Now!!', [], ['class' => 'btn btn-primary']) !!}
         </div>
-    @endif
+    </div>
 @endsection
