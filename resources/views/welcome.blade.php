ef7150863ce9a281b0f5263dca226b202c8c75b4
kyoya.takei <kyoya-takei@dac.co.jp>
2019-05-23 00:43:01 +0000

    cordinate welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,11 @@
     @if (Auth::check())
         aaaaaaaa
     @else
-        <div class="text-center">
-            <h1>Welcome task management application!!</h1>
-            {!! link_to_route('signup.get', 'Sign up Now!!', [], ['class' => 'btn btn-primary']) !!}
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>Welcome to task management application!!</h1>
+                {!! link_to_route('signup.get', 'Sign up Now!!', [], ['class' => 'btn btn-primary']) !!}
+            </div>
         </div>
     @endif
 @endsection
